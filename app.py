d9cc7bc237b308382703f740a7fe23baaccccfaa
dani <mail.com>
2026-02-17 16:22:52 +0530

    Fix image tagging

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from EKSs"
+    return "Hello from EKS Faaahhhhh"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
