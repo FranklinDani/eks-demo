cf73072e9fa7402f3570f45c383e9f77bf606e21
dani <mail.com>
2026-02-17 16:37:33 +0530

    Final CI-CD Commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "EKS Running Successfully"
+    return "EKS CI-CD Updated Successfully"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
